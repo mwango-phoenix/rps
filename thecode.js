c7666efeccc7a561e3c595cec11937ac7437830a
Mick <wang.mick12@gmail.com>
2022-03-18 20:55:16 -0400

    added game function

--- thecode.js
+++ thecode.js
@@ -7,9 +7,7 @@ function computerPlay() {
 
 function oneRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (!plays.includes(playerSelection)){
-        return "Hey, that's not a move!"
-    } else if (computerSelection == playerSelection) {
+    if (computerSelection == playerSelection) {
         return "Tie game!";
     } else if (
         (computerSelection == "rock" && playerSelection == "paper") ||
@@ -19,5 +17,21 @@ function oneRound(playerSelection, computerSelection) {
         return ("Congrats, you win! " + playerSelection + " beats " + computerSelection + ".");
     } else {
         return ("Oh no, try again. " + computerSelection + " beats " + playerSelection + ".");
+    }
+}
+
+function game() {
+    let compScore = 0;
+    let playerScore = 0;
+    for (let i=0; i < 5; ++i) {
+        while(true) {
+            let playerSelection = window.prompt("Rock, Paper, or Scissors: ");
+            if (plays.includes(playerSelection)) {
+                break;
+            } 
+            console.log("Hey, that's not a move!");
+        }
+        let computerSelection = computerPlay();
+        oneRound(playerSelection, computerSelection);
     }
 }
